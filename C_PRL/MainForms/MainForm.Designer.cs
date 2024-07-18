cac86b115ccb11a2d18682e5ee2814545d7478b6
VuHope <lienminhhuyenthoai1379@gmail.com>
2024-07-18 12:21:38 +0700

    Add project files.

---
+++ C_PRL/MainForms/MainForm.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace C_PRL.MainForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            pn_show = new Panel();
+            btn_sanpham = new Button();
+            flp_chucnang = new FlowLayoutPanel();
+            btn_BanHang = new Button();
+            btn_QL_Nhanvien = new Button();
+            btn_QL_Calam = new Button();
+            btn_QL_Hoadon = new Button();
+            btn_QL_Khuyenmai = new Button();
+            btn_QLBan = new Button();
+            btn_logout = new Button();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            flp_chucnang.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pn_show
+            // 
+            pn_show.BackColor = Color.MistyRose;
+            pn_show.Location = new Point(282, 2);
+            pn_show.Name = "pn_show";
+            pn_show.Size = new Size(1225, 642);
+            pn_show.TabIndex = 6;
+            // 
+            // btn_sanpham
+            // 
+            btn_sanpham.FlatStyle = FlatStyle.Flat;
+            btn_sanpham.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            btn_sanpham.Location = new Point(3, 58);
+            btn_sanpham.Name = "btn_sanpham";
+            btn_sanpham.Size = new Size(272, 49);
+            btn_sanpham.TabIndex = 0;
+            btn_sanpham.Text = "Sản Phẩm";
+            btn_sanpham.UseVisualStyleBackColor = true;
+            btn_sanpham.Click += pn_SanPham_Click;
+            // 
+            // flp_chucnang
+            // 
+            flp_chucnang.BackColor = Color.FromArgb(128, 255, 128);
+            flp_chucnang.Controls.Add(btn_BanHang);
+            flp_chucnang.Controls.Add(btn_sanpham);
+            flp_chucnang.Controls.Add(btn_QL_Nhanvien);
+            flp_chucnang.Controls.Add(btn_QL_Calam);
+            flp_chucnang.Controls.Add(btn_QL_Hoadon);
+            flp_chucnang.Controls.Add(btn_QL_Khuyenmai);
+            flp_chucnang.Controls.Add(btn_QLBan);
+            flp_chucnang.Controls.Add(btn_logout);
+            flp_chucnang.Location = new Point(1, 179);
+            flp_chucnang.Name = "flp_chucnang";
+            flp_chucnang.Size = new Size(275, 465);
+            flp_chucnang.TabIndex = 7;
+            // 
+            // btn_BanHang
+            // 
+            btn_BanHang.FlatStyle = FlatStyle.Flat;
+            btn_BanHang.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            btn_BanHang.Location = new Point(3, 3);
+            btn_BanHang.Name = "btn_BanHang";
+            btn_BanHang.Size = new Size(272, 49);
+            btn_BanHang.TabIndex = 3;
+            btn_BanHang.Text = "Bán hàng";
+            btn_BanHang.UseVisualStyleBackColor = true;
+            btn_BanHang.Click += btn_BanHang_Click;
+            // 
+            // btn_QL_Nhanvien
+            // 
+            btn_QL_Nhanvien.FlatStyle = FlatStyle.Flat;
+            btn_QL_Nhanvien.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            btn_QL_Nhanvien.Location = new Point(3, 113);
+            btn_QL_Nhanvien.Name = "btn_QL_Nhanvien";
+            btn_QL_Nhanvien.Size = new Size(272, 49);
+            btn_QL_Nhanvien.TabIndex = 1;
+            btn_QL_Nhanvien.Text = "Nhân Viên";
+            btn_QL_Nhanvien.UseVisualStyleBackColor = true;
+            btn_QL_Nhanvien.Click += btn_QL_Nhanvien_Click;
+            // 
+            // btn_QL_Calam
+            // 
+            btn_QL_Calam.FlatStyle = FlatStyle.Flat;
+            btn_QL_Calam.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            btn_QL_Calam.Location = new Point(3, 168);
+            btn_QL_Calam.Name = "btn_QL_Calam";
+            btn_QL_Calam.Size = new Size(272, 49);
+            btn_QL_Calam.TabIndex = 2;
+            btn_QL_Calam.Text = "Chấm Công";
+            btn_QL_Calam.UseVisualStyleBackColor = true;
+            btn_QL_Calam.Click += btn_QL_Calam_Click;
+            // 
+            // btn_QL_Hoadon
+            // 
+            btn_QL_Hoadon.FlatStyle = FlatStyle.Flat;
+            btn_QL_Hoadon.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            btn_QL_Hoadon.Location = new Point(3, 223);
+            btn_QL_Hoadon.Name = "btn_QL_Hoadon";
+            btn_QL_Hoadon.Size = new Size(272, 49);
+            btn_QL_Hoadon.TabIndex = 4;
+            btn_QL_Hoadon.Text = "Hoá đơn";
+            btn_QL_Hoadon.UseVisualStyleBackColor = true;
+            btn_QL_Hoadon.Click += btn_QL_Hoadon_Click;
+            // 
+            // btn_QL_Khuyenmai
+            // 
+            btn_QL_Khuyenmai.FlatStyle = FlatStyle.Flat;
+            btn_QL_Khuyenmai.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            btn_QL_Khuyenmai.Location = new Point(3, 278);
+            btn_QL_Khuyenmai.Name = "btn_QL_Khuyenmai";
+            btn_QL_Khuyenmai.Size = new Size(272, 49);
+            btn_QL_Khuyenmai.TabIndex = 5;
+            btn_QL_Khuyenmai.Text = "Khuyến mãi";
+            btn_QL_Khuyenmai.UseVisualStyleBackColor = true;
+            btn_QL_Khuyenmai.Click += btn_QL_Khuyenmai_Click;
+            // 
+            // btn_QLBan
+            // 
+            btn_QLBan.FlatStyle = FlatStyle.Flat;
+            btn_QLBan.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            btn_QLBan.Location = new Point(3, 333);
+            btn_QLBan.Name = "btn_QLBan";
+            btn_QLBan.Size = new Size(272, 49);
+            btn_QLBan.TabIndex = 6;
+            btn_QLBan.Text = "Bàn";
+            btn_QLBan.UseVisualStyleBackColor = true;
+            btn_QLBan.Click += btn_QLBan_Click;
+            // 
+            // btn_logout
+            // 
+            btn_logout.FlatStyle = FlatStyle.Flat;
+            btn_logout.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            btn_logout.Location = new Point(3, 388);
+            btn_logout.Name = "btn_logout";
+            btn_logout.Size = new Size(272, 49);
+            btn_logout.TabIndex = 7;
+            btn_logout.Text = "Đăng xuất";
+            btn_logout.UseVisualStyleBackColor = true;
+            btn_logout.Click += btn_logout_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.LightSalmon;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(1, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(275, 174);
+            panel1.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(219, 112);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Snap ITC", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(43, 124);
+            label1.Name = "label1";
+            label1.Size = new Size(222, 36);
+            label1.TabIndex = 0;
+            label1.Text = "Coffee Time";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1508, 647);
+            Controls.Add(pn_show);
+            Controls.Add(flp_chucnang);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "MainForm";
+            Text = "Coffew";
+            Load += MainForm_Load;
+            flp_chucnang.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pn_show;
+        private Button btn_sanpham;
+        private FlowLayoutPanel flp_chucnang;
+        private Button btn_BanHang;
+        private Button btn_QL_Nhanvien;
+        private Button btn_QL_Calam;
+        private Button btn_QL_Hoadon;
+        private Button btn_QL_Khuyenmai;
+        private Button btn_QLBan;
+        private Button btn_logout;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+    }
+}
---
+++ C_PRL/MainForms/MainForm.cs
@@ -0,0 +1,113 @@
+﻿using A_DAL.Models;
+using B_BUS.Services;
+using C_PRL.SubForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_PRL.MainForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        NhanvienServices _nv = new NhanvienServices();
+        public void load_form(Form form)
+        {
+            if (pn_show.Controls.Count > 0)
+            {
+                pn_show.Controls.Clear();
+            }
+            form.TopLevel = false;
+            pn_show.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Show();
+        }
+        private void pn_SanPham_Click(object sender, EventArgs e)
+        {
+            QL_Sanpham qL_Sanpham = new QL_Sanpham();
+            qL_Sanpham.BackColor = Color.MistyRose;
+            load_form(qL_Sanpham);
+        }
+
+        private void btn_QL_Nhanvien_Click(object sender, EventArgs e)
+        {
+            QL_NhanVien qL_Nhanvien = new QL_NhanVien();
+            qL_Nhanvien.BackColor = Color.MistyRose;
+            load_form(qL_Nhanvien);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Nhanvien Inv = new Nhanvien();
+            ChucvuServices chuvu = new ChucvuServices();
+            Inv = DangNhap.Role();
+            Guid role = _nv.GetNhanvienList().Where(nv => nv.Manv == Inv.Manv).First().Chucvu;
+            if (chuvu.GetAllChucvu().ToList().Where(cv => cv.Machucvu == role).First().Tenchucvu == "Admin")
+            {
+                
+            }
+            else
+            {
+                btn_QLBan.Visible = false;
+                btn_QL_Nhanvien.Visible = false;
+                btn_sanpham.Visible = false;
+                btn_QL_Calam.Visible = false;
+                //btn_QL_Hoadon.Visible = false;
+                btn_QL_Khuyenmai.Visible = false;
+            }
+        }
+
+        private void btn_QL_Calam_Click(object sender, EventArgs e)
+        {
+            QL_CaLam qL_CaLam = new QL_CaLam();
+            qL_CaLam.BackColor = Color.MistyRose;
+            load_form(qL_CaLam);
+        }
+
+        private void btn_QL_Hoadon_Click(object sender, EventArgs e)
+        {
+            QL_HoaDon qL_HoaDon = new QL_HoaDon();
+            qL_HoaDon.BackColor = Color.MistyRose;
+            load_form(qL_HoaDon);
+        }
+
+        private void btn_BanHang_Click(object sender, EventArgs e)
+        {
+            BanHang banHang = new BanHang();
+            banHang.BackColor = Color.MistyRose;
+            this.Hide();
+            banHang.Show();
+            banHang.FormClosed += (s, args) => this.Show();
+        }
+
+        private void btn_QL_Khuyenmai_Click(object sender, EventArgs e)
+        {
+            QL_KhuyenMai qL_KhuyenMai = new QL_KhuyenMai();
+            qL_KhuyenMai.BackColor = Color.MistyRose;
+            load_form(qL_KhuyenMai);
+        }
+
+        private void btn_QLBan_Click(object sender, EventArgs e)
+        {
+            QL_Ban qL_Ban = new QL_Ban();
+            qL_Ban.BackColor = Color.MistyRose;
+            load_form(qL_Ban);
+        }
+
+        private void btn_logout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
